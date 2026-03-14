233705816e4c569e487d02885911c37ba564a15c
Maryyyhtsagj <merigev9898@gmail.com>
2026-03-14 20:58:07 +0400

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BottomTabBar } from './components/BottomTabBar';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL || ''}>
       <AppRouter />
       <BottomTabBar />
     </BrowserRouter>
